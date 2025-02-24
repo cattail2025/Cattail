5ddc03c19a7e817ef72ce5f938898acd6de3ac53
cattail2025 <amanjap22@gmail.com>
2025-02-24 13:22:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -99,13 +99,12 @@ darkModeToggle.addEventListener("click", () => {
 
 // Display the banner after 3 seconds
 window.onload = function() {
-  setTimeout(function() {
-    document.getElementById('promoBanner').style.display = 'block';
-  }, 3000);  // 3-second delay before showing banner
+    setTimeout(function() {
+        document.getElementById('promoBanner').style.display = 'block'; // Show the banner
+    }, 3000);  // 3-second delay
 };
 
-// Close the banner when the "X" button is clicked
+// Close the banner when the close button is clicked
 document.getElementById('closeBanner').onclick = function() {
-  document.getElementById('promoBanner').style.display = 'none';
-};
-
+    document.getElementById('promoBanner').style.display = 'none'; // Hide the banner
+});
